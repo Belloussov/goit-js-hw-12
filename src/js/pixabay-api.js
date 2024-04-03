7ee7a2049e515e648e6c2d85f623b52cc910edbc
Yevhenii Belov <studybel@yahoo.com>
2024-04-03 23:23:35 +0200

    final update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
-// const instance = axios.create({
-//   baseURL: 'https://pixabay.com/api',
-//   params: {
-//     key: '42985160-4d9b5f931f1b68b881db47de1',
-//     q: inputValue,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//     per_page: 15,
-//   }
-// });
-
-export async function getPhotos(inputValue) {
-  const baseURL = 'https://pixabay.com/api';
+export async function getPhotos(inputValue, currentPage) {
+  const BASE_URL = 'https://pixabay.com';
+  const END_POINT = '/api/';
   const params = {
     key: '42985160-4d9b5f931f1b68b881db47de1',
     q: inputValue,
@@ -21,8 +10,9 @@ export async function getPhotos(inputValue) {
     orientation: 'horizontal',
     safesearch: true,
     per_page: 15,
+    page: currentPage,
   };
-
-  const res = await axios.get(baseURL,{params} );
+  const url = `${BASE_URL}${END_POINT}`;
+  const res = await axios.get(url, { params });
   return res.data;
 }
--- src/main.js
+++ src/main.js
@@ -6,20 +6,56 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 import axios from 'axios';
 
-console.log(axios.isCancel('something'));
+//console.log(axios.isCancel('something'));
 
 import { getPhotos } from './js/pixabay-api';
 import { photosTemplate } from './js/render-functions';
 
+//===============================variables=================================
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.btn');
+const preloader = document.querySelector('.loader');
+let inputValue;
+let currentPage = 1;
+let maxPage = 0;
+const perPage = 15;
+
 let lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+
+//=============================function===========================================
+
 lightbox.on('show.simplelightbox', function () {});
 
-const preloader = document.querySelector('.loader');
+function checkBtnStatus() {
+  if (currentPage >= maxPage) {
+    hideLoadMore();
+    iziToast.warning({
+      message: 'We are sorry, but you have reached the end of search results.',
+      color: 'blue',
+      position: 'topRight',
+    });
+  } else {
+    showLoadMore();
+  }
+}
+function myScroll() {
+  const height = gallery.firstChild.getBoundingClientRect().height;
+  console.log(height);
+  scrollBy({
+    top: height * 2,
+    behavior: 'smooth',
+  });
+}
+function showLoadMore() {
+  return btnLoadMore.classList.remove('is-hidden');
+}
+function hideLoadMore() {
+  return btnLoadMore.classList.add('is-hidden');
+}
 
 function showLoader() {
   return preloader.classList.remove('is-hidden');
@@ -28,11 +64,12 @@ function hideLoader() {
   return preloader.classList.add('is-hidden');
 }
 hideLoader();
+hideLoadMore();
 
 form.addEventListener('submit', async e => {
   e.preventDefault();
 
-  const inputValue = e.target.elements.search.value.trim();
+  inputValue = e.target.elements.search.value.trim();
   if (inputValue === '') {
     return iziToast.warning({
       message: 'Please complete the field!',
@@ -41,9 +78,11 @@ form.addEventListener('submit', async e => {
     });
   }
   gallery.innerHTML = '';
+  currentPage = 1;
   showLoader();
   try {
-    const photos = await getPhotos(inputValue);
+    const photos = await getPhotos(inputValue, currentPage);
+    maxPage = Math.ceil(photos.totalHits / perPage);
     const markup = photosTemplate(photos.hits);
     if (photos.hits.length === 0) {
       iziToast.error({
@@ -52,9 +91,11 @@ form.addEventListener('submit', async e => {
         position: 'topRight',
         color: 'red',
       });
+      checkBtnStatus();
     } else {
       gallery.insertAdjacentHTML('beforeend', markup);
       lightbox.refresh();
+      checkBtnStatus();
     }
   } catch (error) {
     iziToast.error({
@@ -67,4 +108,16 @@ form.addEventListener('submit', async e => {
     hideLoader();
     form.reset();
   }
+});
+
+btnLoadMore.addEventListener('click', async e => {
+  showLoader();
+  currentPage += 1;
+  const photos = await getPhotos(inputValue, currentPage);
+  const markup = photosTemplate(photos.hits);
+  gallery.insertAdjacentHTML('beforeend', markup);
+  myScroll();
+  lightbox.refresh();
+  hideLoader();
+  checkBtnStatus();
 });
